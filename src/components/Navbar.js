118dd4146bf06e4cb7c41ffab4a18c9b6dbc33b9
Ben Cruise <ben_cruise@hotmail.co.uk>
2023-06-09 20:33:54 +0100

    adding all images and finalising CSS for medium and smaller screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,31 +9,46 @@ import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons"
 function Navbar({setChosenPage}) {
   
   return (
-    <div className="nav-bar">
-      <div className="container navbar-container">
-        <div className="col-12 top">
-          <div className="col-4">
-            <a href="https://www.etsy.com/uk/shop/ArtbyNatashaStudio?ref=profile_header">
-              <FontAwesomeIcon className="link bag" icon={faBagShopping} />
-            </a>
+    <>
+      <div className="nav-bar">
+        <div className="container navbar-container">
+          <div className="col-12 top">
+            <div className="col-4">
+              <a href="https://www.etsy.com/uk/shop/ArtbyNatashaStudio?ref=profile_header">
+                <FontAwesomeIcon className="link bag wide" icon={faBagShopping} />
+              </a>
+            </div>
+            <img className="col-4" src={Logo} onClick={() => setChosenPage('Home')}></img>
+            <div className="col-4">
+              <a href="https://m.facebook.com/100063269947728">
+                <FontAwesomeIcon className="link m-1 facebook wide" icon={faFacebook} />
+              </a>
+              <a href="https://www.instagram.com/tash.dudley/?h;=en">
+                <FontAwesomeIcon className="link m-1 instagram wide" icon={faInstagram} />
+              </a>
+            </div>
           </div>
-          <img className="col-4" src={Logo} onClick={() => setChosenPage('Home')}></img>
-          <div className="col-4">
-            <a href="https://m.facebook.com/100063269947728">
-              <FontAwesomeIcon className="link m-1 facebook" icon={faFacebook} />
-            </a>
-            <a href="https://www.instagram.com/tash.dudley/?h;=en">
-              <FontAwesomeIcon className="link m-1 instagram" icon={faInstagram} />
-            </a>
+          <div className="col-12 thin">
+            <div className='col-6 thin-div'>
+              <a href="https://www.etsy.com/uk/shop/ArtbyNatashaStudio?ref=profile_header">
+                <FontAwesomeIcon className="link m-1 bag" icon={faBagShopping} />
+              </a>
+              <a href="https://m.facebook.com/100063269947728">
+                <FontAwesomeIcon className="link m-1 facebook" icon={faFacebook} />
+              </a>
+              <a href="https://www.instagram.com/tash.dudley/?h;=en">
+                <FontAwesomeIcon className="link m-1 instagram" icon={faInstagram} />
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="col-10 bottom">
-          {data.map(item => 
-            <span className="col-4" onClick={() => setChosenPage(item.title)}>{item.title}</span>  
-          )}
+          <div className="col-10 bottom">
+            {data.map(item => 
+              <span className="col-4" onClick={() => setChosenPage(item.title)}>{item.title}</span>  
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/data/images.js
+++ src/data/images.js
@@ -1,24 +1,49 @@
 //Images
-import childrensBooksImg1 from '../images/Calendar.png';
-import patternsImg1 from '../images/PlanMyDinnerPicture.png';
-import commissionsImg1 from '../images/ReadMe.png';
+import CB1 from '../images/Childrens Books/CB1.jpg';
+import CB2 from '../images/Childrens Books/CB2.jpg';
+import CB3 from '../images/Childrens Books/CB3.jpg';
+import CB4 from '../images/Childrens Books/CB4.jpg';
+import CB5 from '../images/Childrens Books/CB5.jpg';
+import P1 from '../images/Patterns/P1.jpg';
+import P2 from '../images/Patterns/P2.jpg';
+import P3 from '../images/Patterns/P3.jpg';
+import P4 from '../images/Patterns/P4.jpg';
+import P5 from '../images/Patterns/P5.jpg';
+import C1 from '../images/Commissions/C1.jpg';
+import C2 from '../images/Commissions/C2.jpg';
+import C3 from '../images/Commissions/C3.jpg';
+import C4 from '../images/Commissions/C4.jpg';
+import C5 from '../images/Commissions/C5.jpg';
+import C6 from '../images/Commissions/C6.jpg';
+import C7 from '../images/Commissions/C7.jpg';
+import C8 from '../images/Commissions/C8.jpg';
+import C9 from '../images/Commissions/C9.jpg';
+import C10 from '../images/Commissions/C10.jpg';
+import C11 from '../images/Commissions/C11.jpg';
+import C12 from '../images/Commissions/C12.jpg';
+import C13 from '../images/Commissions/C13.jpg';
+import C14 from '../images/Commissions/C14.jpg';
 
 const allImages = [
     {
         "title": "childrens books",
-        "pictures": [childrensBooksImg1, childrensBooksImg1]
-    },
-    {
-        "title": "patterns",
-        "pictures": [patternsImg1]
+        "pictures": [CB1, CB2, CB3, CB4, CB5]
     },
     {
         "title": "commissions",
-        "pictures": [commissionsImg1]
+        "pictures": [C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12,C13,C14]
+    },
+    {
+        "title": "patterns",
+        "pictures": [P1,P2,P3,P4,P5]
     },
     {
         "title": "all images",
-        "pictures": [{image: childrensBooksImg1, id: 0}, {image: patternsImg1, id: 1}, {image: commissionsImg1, id: 2}]
+        "pictures": [{image: CB1, id: 0}, {image: CB2, id: 1}, {image: CB3, id: 2}, {image: CB4, id: 3}, {image: CB5, id: 4}, 
+                     {image: P5, id: 5}, {image: P4, id: 6}, {image: P3, id: 7}, {image: P4, id: 8}, {image: P5, id: 9},
+                     {image: C1, id: 10}, {image: C2, id: 11}, {image: C3, id: 12}, {image: C4, id: 13}, {image: C5, id: 14},
+                     {image: C6, id: 15}, {image: C7, id: 16}, {image: C8, id: 17}, {image: C9, id: 18}, {image: C10, id: 19},
+                     {image: C11, id: 20}, {image: C12, id: 21}, {image: C13, id: 22}, {image: C14, id: 23}]
     }
 ]
 
